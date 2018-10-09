e7eb22e2729fd600d8e4fd6f8a7cafe38acf0abb
Arthit Suriyawongkul <arthit@gmail.com>
2018-10-09 15:04:55 +0700

    - thaitokenize.py script to run before txt2tajson.py - config file for Thai language

---
+++ data/thaitokenize.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+import codecs
+import os
+import sys
+
+from pythainlp import word_tokenize
+
+# This file converts a folder full of text files into a folder of space-tokenized files.
+
+# Usage:
+# $ thaitokenize.py input_folder output_folder
+
+
+def convert(infolder, outfolder):
+    if not os.path.exists(outfolder):
+        os.mkdir(outfolder)
+
+    for fname in os.listdir(infolder):
+        with open(infolder + "/" + fname) as f:
+            lines = f.readlines()
+        with codecs.open(outfolder + "/" + fname, "w", encoding="utf-8") as out:
+            for line in lines:
+                tokens = [tok for tok in word_tokenize(line) if tok != " "]
+                out.write(" ".join(tokens))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: thaitokenize.py input_folder output_folder")
+        exit(1)
+
+    infolder = sys.argv[1]
+    outfolder = sys.argv[2]
+    convert(infolder, outfolder)
